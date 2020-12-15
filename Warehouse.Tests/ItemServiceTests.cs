f3c9f7cc1e1abdc49ddc5a374f231d334e255f05
Łukasz Grzejszczyk <grzejszczyk@gmail.com>
2020-12-15 13:55:45 +0100

    Initial Test implementation. Automapper issue.

--- Warehouse.Tests/ItemServiceTests.cs
+++ Warehouse.Tests/ItemServiceTests.cs
@@ -21,16 +21,29 @@ namespace Warehouse.Tests
         {
             //Arrange:
 
-            //Mocking ItemRepository with methot GetItemById.
-            //Returns Item.
+            var mockRepo = new Mock<IItemRepository>();
+            mockRepo.Setup(s => s.GetItemById(1))
+                .Returns(GetTestItem());
+
+            var item = mockRepo.Object.GetItemById(1);
+
+            var mockMapperItemVM = new Mock<IMapper>();
+            mockMapperItemVM.Setup(s => s.Map<ItemDetailsVM>(item));
+            //issue: itemVM is null in ItemService.
+
+            var itemService = new ItemService(mockRepo.Object, null, null, mockMapperItemVM.Object);
 
             //Act:
-
-            //???
+            var result = itemService.GetItemDetails(1);
 
             //Assert:
+            Assert.Equal(result.Id, mockRepo.Object.GetItemById(1).Id);
+        }
 
-            //Propierties from Item equals ItemDetailsVM
+        private Item GetTestItem()
+        {
+            var item = new Item() { Id = 1, Name = "TestItem1" };
+            return item;
         }
     }
 }
