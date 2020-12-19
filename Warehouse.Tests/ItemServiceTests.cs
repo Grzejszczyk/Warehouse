9509d658b4721d64c8ea863d5479fc2c6884c6ab
Łukasz Grzejszczyk <grzejszczyk@gmail.com>
2020-12-19 12:13:19 +0100

    New Test for SupplierService - GetSupplierDetails. Unsolved.

--- Warehouse.Tests/ItemServiceTests.cs
+++ Warehouse.Tests/ItemServiceTests.cs
@@ -12,6 +12,7 @@ using Warehouse.Application.Interfaces;
 using Moq;
 using Warehouse.Infrastructure.Repositories;
 
+
 namespace Warehouse.Tests
 {
     public class ItemServiceTests
@@ -21,29 +22,15 @@ namespace Warehouse.Tests
         {
             //Arrange:
 
-            var mockRepo = new Mock<IItemRepository>();
-            mockRepo.Setup(s => s.GetItemById(1))
-                .Returns(GetTestItem());
-
-            var item = mockRepo.Object.GetItemById(1);
-
-            var mockMapperItemVM = new Mock<IMapper>();
-            mockMapperItemVM.Setup(s => s.Map<ItemDetailsVM>(item));
-            //issue: itemVM is null in ItemService.
-
-            var itemService = new ItemService(mockRepo.Object, null, null, mockMapperItemVM.Object);
-
             //Act:
-            var result = itemService.GetItemDetails(1);
 
             //Assert:
-            Assert.Equal(result.Id, mockRepo.Object.GetItemById(1).Id);
         }
 
-        private Item GetTestItem()
+        private Supplier GetTestItem()
         {
-            var item = new Item() { Id = 1, Name = "TestItem1" };
-            return item;
+            var supplier = new Supplier() { Id = 1, Name = "TestSupplier1" };
+            return supplier;
         }
     }
 }
---
+++ Warehouse.Tests/SupplierServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.Application.Services;
+using Warehouse.Application.ViewModels.Supplier;
+using Warehouse.Domain.Interfaces;
+using Warehouse.Domain.Models.Entity;
+using Xunit;
+
+namespace Warehouse.Tests
+{
+    public class SupplierServiceTests
+    {
+        private readonly SupplierService _supplierService;
+        private readonly Mock<ISupplierRepository> _supplierRepositoryMock = new Mock<ISupplierRepository>();
+        private readonly Mock<IMapper> _mapperMock = new Mock<IMapper>();
+
+        //TODO: Solve Test for Mapping.
+
+        public SupplierServiceTests()
+        {
+            _supplierService = new SupplierService(_supplierRepositoryMock.Object, _mapperMock.Object);
+        }
+
+        [Fact]
+        public void GetSupplierDetails_Test()
+        {
+            //Arrange:
+            var supplier = new Supplier()
+            {
+                Id = 1,
+                Name = "TestSupplier1"
+            };
+
+            _supplierRepositoryMock.Setup(s => s.GetSupplierById(1))
+                .Returns(supplier);
+
+            //Act:
+            var result = _supplierService.GetSupplierDetails(1);
+
+            //Assert:
+            Assert.Equal(result.Id, supplier.Id);
+        }
+    }
+}
